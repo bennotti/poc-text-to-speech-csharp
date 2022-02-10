737183c881a15dd0cb76c25286bf3d25f890c43d
Bruno Bennotti <brunobennotti@gmail.com>
2022-02-10 18:51:58 -0300

    estrutura inicial

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Speech.Synthesis;
+
+namespace poc_text_to_speech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            synthesizer.Rate = 3;
+            var builder = new PromptBuilder();
+            builder.StartVoice(new CultureInfo("pt-BR"));
+            builder.AppendText("BORA BORA - CORRE QUE TA ACABANDO!!");
+            builder.EndVoice();
+            synthesizer.Speak(builder);
+            Console.WriteLine("FOI");
+        }
+    }
+}
